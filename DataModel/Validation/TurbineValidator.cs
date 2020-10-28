d73901e82848467f38327598edce7d64f389f514
Olly Maunder <oliver.maunder@dnvgl.com>
2020-10-28 12:35:50 +0000

    Added validation of inputs using FluentValidation

---
+++ DataModel/Validation/TurbineValidator.cs
@@ -0,0 +1,18 @@
+using DataModel.Dto;
+using FluentValidation;
+
+namespace DataModel.Validation
+{
+    public class TurbineValidator : AbstractValidator<Turbine>
+    {
+        public TurbineValidator()
+        {
+            RuleFor(t => t.BladeLength)
+                .GreaterThan(0.0)
+                .LessThan(t => t.TowerHeight).WithMessage("Blade length must be shorter than tower height");
+                
+            RuleFor(t => t.TowerHeight).GreaterThan(0.0);
+            RuleFor(t => t.Location).SetValidator(new LocationValidator());
+        }
+    }
+}
