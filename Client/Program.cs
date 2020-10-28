596881cdf4ac6b61c62eac7e5c6bbd2dd7bc3b33
Olly Maunder <oliver.maunder@dnvgl.com>
2020-10-28 12:35:50 +0000

    Added validation of inputs using FluentValidation

--- Client/Program.cs
+++ Client/Program.cs
@@ -29,28 +29,43 @@ namespace Client
             };
 
             var calculation = new HeightCalculation();
-            Console.WriteLine($"Average turbine height: {calculation.AverageTurbineHeight(windFarm)}m");
-
+            
+            try
+            {
+                Console.WriteLine($"Average turbine height: {calculation.AverageTurbineHeight(windFarm)}m");
+            }
+            catch (FluentValidation.ValidationException e)
+            {
+                PrintValidationException(e);
+            }
+            
             try
             {
                 WindFarm emptyFarm = new WindFarm(); 
-                // Throws ArgumentNull because the turbine list is null
                 Console.WriteLine($"Average turbine height in empty windfarm: {calculation.AverageTurbineHeight(emptyFarm)}m");
             }
-            catch (Exception e)
+            catch (FluentValidation.ValidationException e)
             {
-                Console.WriteLine($"Caught {e.GetType().Name}");
+                PrintValidationException(e);
             }
 
             try
             {
                 WindFarm emptyFarm = new WindFarm{ Turbines = Array.Empty<Turbine>()}; 
-                // Returns NaN because the turbine list is empty
                 Console.WriteLine($"Average turbine height in empty windfarm: {calculation.AverageTurbineHeight(emptyFarm)}m");
             }
-            catch (Exception e)
+            catch (FluentValidation.ValidationException e)
             {
-                Console.WriteLine($"Caught {e.GetType().Name}");
+                PrintValidationException(e);
+            }
+            
+            void PrintValidationException(FluentValidation.ValidationException e)
+            {
+                Console.WriteLine("Validation failed");
+                foreach (var error in e.Errors)
+                {
+                    Console.WriteLine($" - {error.ErrorMessage}");
+                }
             }
         }
     }
---
+++ DataModel/Validation/WindFarmValidator.cs
@@ -0,0 +1,15 @@
+using DataModel.Dto;
+using FluentValidation;
+
+namespace DataModel.Validation
+{
+    public class WindFarmValidator : AbstractValidator<WindFarm>
+    {
+        public WindFarmValidator()
+        {
+            RuleFor(wf => wf.Location).SetValidator(new LocationValidator());
+            RuleFor(wf => wf.Turbines).NotEmpty();
+            RuleForEach(wf => wf.Turbines).SetValidator(new TurbineValidator());
+        }
+    }
+}
