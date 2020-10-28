fed2eb753e7ce0cd6ab3e27b3a5f772c5a7bab05
Olly Maunder <oliver.maunder@dnvgl.com>
2020-10-28 12:35:50 +0000

    Added validation of inputs using FluentValidation

--- Calculation/HeightCalculation.cs
+++ Calculation/HeightCalculation.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
 using DataModel.Dto;
+using DataModel.Validation;
+using FluentValidation;
+using FluentValidation.Results;
 
 namespace Calculation
 {
@@ -8,6 +11,11 @@ namespace Calculation
     {
         public double AverageTurbineHeight(WindFarm windFarm)
         {
+            // Validate the inputs. Need to do this in the calculation because we
+            // can't assume that the caller has validated the inputs.
+            var validator = new WindFarmValidator();
+            validator.ValidateAndThrow(windFarm);
+            
             double totalHeight = windFarm.Turbines.Sum(t => t.TowerHeight + t.BladeLength);
             return totalHeight / windFarm.Turbines.Count; 
         }
---
+++ DataModel/Validation/LocationValidator.cs
@@ -0,0 +1,14 @@
+using DataModel.Dto;
+using FluentValidation;
+
+namespace DataModel.Validation
+{
+    public class LocationValidator : AbstractValidator<Location>
+    {
+        public LocationValidator()
+        {
+            RuleFor(l => l.Latitude).InclusiveBetween(-90, 90);
+            RuleFor(l => l.Longitude).InclusiveBetween(-180, 180);
+        }
+    }
+}
